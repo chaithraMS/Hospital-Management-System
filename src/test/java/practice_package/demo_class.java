bd353dd8b96e74ee74843d1c69dc1fb8dd0d9bce
ROOMAN <ROOMAN@LAPTOP-LPRDJ66B>
2022-11-21 16:22:18 +0530

    demo and sample committed

--- src/test/java/practice_package/demo_class.java
+++ src/test/java/practice_package/demo_class.java
@@ -3,6 +3,10 @@ package practice_package;
 public class demo_class {
 
 	public static void main(String[] args) {
+		
+		
+		
+		
 		System.out.println("RoomanSheriff");
 
 	}
--- src/test/java/practice_package/sample1234.java
+++ src/test/java/practice_package/sample1234.java
@@ -4,6 +4,9 @@ public class sample1234 {
 
 	public static void main(String[] args) {
 		System.out.println("aliya alu");
+		
+		
+		
 
 	}
 
