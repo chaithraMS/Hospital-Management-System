5effce03cd3c390d7b29b7e668626b3f2eda9dee
DELL <DELL@DESKTOP-7SNL7GR>
2022-11-21 17:55:19 +0530

    asds

--- src/test/java/practice_package/CreateTest.java
+++ src/test/java/practice_package/CreateTest.java
@@ -5,6 +5,9 @@ public class CreateTest {
 	public static void main(String[] args) {
 		
 		System.out.println("create successfully");
+		
+		System.out.println("updated");
+		System.out.println("apple");
 
 	}
 
