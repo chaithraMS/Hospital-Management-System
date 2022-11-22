6467172598b8bf5f2049c306eb2f92543f2943d3
chaithraMS <118715227+chaithraMS@users.noreply.github.com>
2022-11-22 16:56:00 +0530

    Update CreateTest.java

--- src/test/java/practice_package/CreateTest.java
+++ src/test/java/practice_package/CreateTest.java
@@ -5,9 +5,6 @@ public class CreateTest {
 	public static void main(String[] args) {
 		
 		System.out.println("create successfully");
-		
-		System.out.println("updated");
-		System.out.println("apple");
 
  
 	}
